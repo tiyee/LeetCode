c1838b0eb45ee38d4b62a03834ce66138e108b8e
ijiangtao <ijiangtao@didichuxing.com>
2019-02-25 15:48:49 +0800

    atio

---
+++ src/median-of-two-sorted-arrays/solution.cpp
@@ -0,0 +1,4 @@
+class Solution {
+ public:
+  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {}
+};
--- src/string-to-integer-atoi/solution.cpp
+++ src/string-to-integer-atoi/solution.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 class Solution {
  private:
@@ -6,24 +7,47 @@ class Solution {
     if (x < '0' || x > '9') return -1;
     return x - '0';
   }
+  int raise(long ret) {
+    if (ret < INT_MIN) return INT_MIN;
+    if (ret > INT_MAX) return INT_MAX;
+    return static_cast<int>(ret);
+  }
 
  public:
-  int myAtoi(string& str) {
-    int n = 0;
-    for (unsigned long i = 0; i < str.length(); ++i) {
-      if (n == 0) {
-        if (s[i] != ' ') {
-          continue;
+  int myAtoi(string s) {
+    bool bBegin = false;
+    long n = 0;
+    int sep = 1;
+
+    for (auto item : s) {
+      if (!bBegin && item == ' ') continue;
+      if (!bBegin) {
+        if (item == '-') {
+          sep *= -1;
+          bBegin = true;
+
+        } else if (item == '+') {
+          bBegin = true;
+
+        } else if (toNumber(item) == -1) {
+          return false;
+        } else {
+          n = toNumber(item);
+          bBegin = true;
         }
-        if (s[i] == '+') {
-          continue;
-        }
-        if (s[i] == '-') {
-          n = -1;
-        }
+
       } else {
-        auto j = toNumber(s[i]);
+        auto j = toNumber(item);
+        if (j == -1) return raise(n * sep);
+        n = (n * 10) + j;
+        if (n > INT_MAX) break;
       }
     }
+    return raise(n * sep);
   }
 };
+int main() {
+  Solution sol;
+
+  cout << sol.myAtoi("  -0012a42") << endl;
+}
